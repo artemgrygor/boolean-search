729e6cd1931784a8cd4beddbdc8d222f392723a2
Artem Grygor <artemgrygor@gmail.com>
2014-04-21 14:45:00 -0400

    init service provider

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,47 @@ var paths = {
 	app : './source/boolean-search.js',
 	specs : './test/*.js'
 }
+
+function runKarma(karmaConf, options){
+	var config = {
+		configFile: karmaConf,
+	      action: 'watch'
+	};
+
+	Object.keys(options).forEach(function(key) {
+      config[key] = options[key];
+    });
+
+    return gulp.src(['no need to supply files because everything is in config file'])
+	    .pipe(karma(config).on('error', handleError));
+}
+
+
+var handleError = function (err) {
+  console.log(err.name, ' in ', err.plugin, ': ', err.message);
+  this.emit('end');
+};
+
+gulp.task('karma', function  (argument) {
+	return runKarma(paths.karmaConf,{action: 'watch'});
+});
+
+gulp.task('lint',function(){
+	return gulp.src([paths.app, paths.specs])
+		   .pipe(jshint())
+		   .pipe(jshint.reporter("default"))
+		   .on('error', handleError);
+});
+
+gulp.task('karma-single',function(){
+	return runKarma(paths.karmaConf,{
+			action: 'run',
+	      autoWatch : false,
+	      singleRun : true});
+});
+
+gulp.task('watch', function(){
+	gulp.watch([paths.app, paths.specs],['lint', 'karma-single']);
+});
+
+gulp.task('default',['lint', 'karma-single', 'watch']);
--- source/ngBooleanSearch.js
+++ source/ngBooleanSearch.js
@@ -1,1 +1,21 @@
-angular.module('ngBooleanSearch', ['ng']);
+(function (window, angular, undefined) {
+	'use strict';
+
+	var module angular.module('ngBooleanSearch', []);
+	module.provider('ngBooleanSearch', function(){
+
+		this.$get = ['$document', '$templateCache', '$compile', '$q', '$http', '$rootScope', '$timeout',
+			function ($document, $templateCache, $compile, $q, $http, $rootScope, $timeout) {
+
+				var publicMethods = {
+
+					test: function(){
+						return 42;
+					}
+				};
+
+				return publicMethods;
+			}];
+
+	});	
+})(window, window.angular);
